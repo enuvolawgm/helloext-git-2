7e843ffe6517992ddf82e70795d646ee1b35576b
enuvolaw <w.villa@enuvola.com>
2013-09-10 15:26:55 +0200

    2-Trasformato in panel e aggiunto bottone

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ Ext.require('Ext.container.Viewport');
 Ext.application({
     name: 'HelloExt',
     launch: function() {
-        Ext.create('Ext.container.Viewport', {
+        Ext.create('Ext.panel.Panel', {
             layout: 'fit',
-            items: [
-                {
-                    title: 'Hello Ext',
-                    html : 'Hello! Welcome to Ext JS.'
-                }
-            ]
+            title: 'Hello Ext',
+            html : 'Hello! Welcome to Ext JS.',
+            buttons:[{
+            	text: 'Click me',
+				handler: function() {
+							alert('You clicked the button!');
+				}
+            }],
+            renderTo: Ext.getBody(),
         });
     }
 });
